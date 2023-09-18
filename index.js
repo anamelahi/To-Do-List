d94e4351f742fa7d542d01e12295888adf7f0244
anamelahi <anamelahi04@gmail.com>
2023-09-18 16:05:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res)=>{
+    res.render("header.ejs");
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is running on ${port}`);
+})
+
+
